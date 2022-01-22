388b7faddcbb496347e515b1669b49e6b5ad8c70
Shubham <ssakhuja@aramse.io>
2022-01-22 14:41:57 -0500

    update

--- src/io/aramse/f8/Utils.groovy
+++ src/io/aramse/f8/Utils.groovy
@@ -6,7 +6,7 @@ class Utils {
   }
 
   static def getMergedBranch(){
-    msg = this.getCommitMsg()
+    def msg = this.getCommitMsg()
     return msg.startsWith('Merge pull request') ? msg.split('/').last() : null
   }
 
@@ -14,7 +14,7 @@ class Utils {
     return slackUserIdFromEmail(sh(script: 'git --no-pager show -s --format=\'%ae\'', returnStdout: true).trim())
   }
 
-  static def slackNotify(String result) {
-    slackSend(color: result == 'SUCCESS' ? '#4ee44e' : '#ff0000', message: "${JOB_NAME} <${BUILD_URL}/display/redirect|Build #${BUILD_NUMBER}> " + result + " (last committer: <@" + this.getCommitter() + ">)")
+  static def slackNotify(script, result) {
+    slackSend(color: result == 'SUCCESS' ? '#4ee44e' : '#ff0000', message: "${script.env.JOB_NAME} <${script.env.BUILD_URL}/display/redirect|Build #${script.env.BUILD_NUMBER}> " + result + " (last committer: <@" + this.getCommitter() + ">)")
   }
 }
