1f9c4cfad5b532f61cbdc18900883ff7bc6f1df0
Shubham <ssakhuja@aramse.io>
2022-01-22 14:52:00 -0500

    update

--- src/io/aramse/f8/Utils.groovy
+++ src/io/aramse/f8/Utils.groovy
@@ -1,20 +1,20 @@
 package io.aramse.f8
 
 class Utils {
-  static def getCommitMsg() {
-    return sh(script: 'git log -1 --pretty=%B', returnStdout: true).split('\n').first().trim()
+  static def getCommitMsg(script) {
+    return script.sh(script: 'git log -1 --pretty=%B', returnStdout: true).split('\n').first().trim()
   }
 
-  static def getMergedBranch(){
-    def msg = this.getCommitMsg()
+  static def getMergedBranch(script){
+    def msg = this.getCommitMsg(script)
     return msg.startsWith('Merge pull request') ? msg.split('/').last() : null
   }
 
-  static def getCommitter() {
-    return slackUserIdFromEmail(sh(script: 'git --no-pager show -s --format=\'%ae\'', returnStdout: true).trim())
+  static def getCommitter(script) {
+    return script.slackUserIdFromEmail(script.sh(script: 'git --no-pager show -s --format=\'%ae\'', returnStdout: true).trim())
   }
 
   static def slackNotify(script, result) {
-    slackSend(color: result == 'SUCCESS' ? '#4ee44e' : '#ff0000', message: "${script.env.JOB_NAME} <${script.env.BUILD_URL}/display/redirect|Build #${script.env.BUILD_NUMBER}> " + result + " (last committer: <@" + this.getCommitter() + ">)")
+    slackSend(color: result == 'SUCCESS' ? '#4ee44e' : '#ff0000', message: "${script.env.JOB_NAME} <${script.env.BUILD_URL}/display/redirect|Build #${script.env.BUILD_NUMBER}> " + result + " (last committer: <@" + this.getCommitter(script) + ">)")
   }
 }
